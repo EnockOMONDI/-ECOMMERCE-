953b8edcd5d32872d154aadb7b585d54fbbcb47f
enockomondi <djseanizellkenya@gmail.com>
2018-07-12 02:12:36 +0300

    added a default  navbar

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,4 +23,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     path('', include('shop.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
