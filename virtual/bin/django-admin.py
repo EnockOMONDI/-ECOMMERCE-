2ec42f7e5a33c5a9005ce4201bf412415ab3ae69
enockomondi <djseanizellkenya@gmail.com>
2018-07-12 00:13:54 +0300

    initial commit

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sean/Desktop/PROJECTS/PERSONAL PROJECTS/After school/DJANGO ECOMMERCE/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
