b09b12b640646626ee48533ee36852595c10e22f
enockomondi <djseanizellkenya@gmail.com>
2018-07-12 01:24:54 +0300

    configured my app urls in my project

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
