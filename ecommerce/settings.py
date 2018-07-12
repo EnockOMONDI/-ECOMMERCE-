f207316316943667e49284269c9861dc8d3db830
enockomondi <djseanizellkenya@gmail.com>
2018-07-12 03:01:32 +0300

    created a cart app to manage shopping cart

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
