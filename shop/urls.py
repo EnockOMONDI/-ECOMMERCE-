5917cb287b6b25a81ebef26c4c94904b2d7a9416
enockomondi <djseanizellkenya@gmail.com>
2018-07-20 10:19:22 +0300

    configured shop urls

--- shop/urls.py
+++ shop/urls.py
@@ -9,10 +9,10 @@ app_name = 'shop'
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^about/', views.about, name='about/'),
-    url(r'^contact/', views.contact, name='contact/'),
-    url(r'^mens/', views.mens, name='mens/'),
-    url(r'^women/', views.womens, name='womens/'),
+    url(r'^about/', views.about, name='about'),
+    url(r'^contact/', views.contact, name='contact'),
+    url(r'^mens/', views.mens, name='mens'),
+    url(r'^women/', views.womens, name='womens'),
     url(r'^shop/', views.product_list, name='product_list'),
     url(r'^(?P<category_slug>[-\w]+)/$', views.product_list, name='product_list_by_category'),
     url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail'),
