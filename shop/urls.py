39d13f4b562544664b3e055be116810a2c660592
enockomondi <djseanizellkenya@gmail.com>
2018-07-22 22:41:00 +0300

    linked static files

--- shop/urls.py
+++ shop/urls.py
@@ -17,4 +17,4 @@ urlpatterns = [
     url(r'^(?P<category_slug>[-\w]+)/$', views.product_list, name='product_list_by_category'),
     url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail'),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
